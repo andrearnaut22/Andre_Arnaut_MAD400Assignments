e81edbc4b330a09059108581520a6c789ec8c44f
Andre Arnaut <aa430@myscc.ca>
2023-01-26 01:06:45 -0500

    Generated interface, created ContentList, and updated interface info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IContent } from './models/icontent';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,19 @@ export class AppComponent {
   id = 1;
   title = 'Mixed Martial Arts';
   author = 'Andre Arnaut';
+  martialArt: IContent;
+
+ constructor() {
+   this.martialArt = {
+     id: 1025,
+     imageURL: '',
+     body: '<p>Mixed martial arts is a great workout, a great sport, and a great way to learn how to defend yourself.</p>'
+   };
+   this.processContent(this.martialArt);
+ } 
+
+ processContent(content: IContent):string {
+   console.log(content.body);
+   return content.type ?? "EMPTY";
+ }
 }
